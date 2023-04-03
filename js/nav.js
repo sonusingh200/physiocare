d9a483cf581da3b0daec473617acd188ddc335e9
sonusingh <sonusinghcom926@gmail.com>
2023-04-03 10:54:51 +0530

    done

--- js/nav.js
+++ js/nav.js
@@ -2,9 +2,19 @@ let openBtn = document.getElementById('open-menu')
 let closeBtn = document.getElementById('close-menu')
 let navMenu = document.getElementById('nav-bar-container')
 
-openBtn.addEventListener('click', ()=>{
+openBtn.addEventListener('click', () => {
     navMenu.classList.add('show')
 })
-closeBtn.addEventListener('click', ()=>{
+closeBtn.addEventListener('click', () => {
     navMenu.classList.remove('show')
+})
+
+
+
+let navLinks = document.querySelectorAll('.menu-links')
+navLinks.forEach(e => {
+    e.addEventListener('click', () => {
+        let navMenu = document.getElementById('nav-bar-container')
+        navMenu.classList.remove('show')
+    })
 })
