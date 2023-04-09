8565592a7b7e5c95e1ee7225289399bc508f563b
sonusingh <sonusinghcom926@gmail.com>
2023-04-09 14:10:33 +0530

    jj

--- js/nav.js
+++ js/nav.js
@@ -20,12 +20,11 @@ navLinks.forEach(e => {
     
 })
 
-// let scrollh = () => {
-//     let header = document.getElementById("header");
-//     this.scrollY >= 50
-//       ? header.classList.add("bg-header")
-//       : header.classList.remove("bg-header");
-//   };
-
-//   window.addEventListener("scroll", scrollh);
-
+let scrollh = () => {
+    let header = document.getElementById("header");
+    this.scrollY >= 50
+      ? header.classList.add("bg-header")
+      : header.classList.remove("bg-header");
+  };
+  
+  window.addEventListener("scroll", scrollh);
