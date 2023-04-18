c5827c660059a839a602460225d9fb9f4d56aef3
sonusingh <sonusinghcom926@gmail.com>
2023-04-18 12:19:58 +0530

    ff

--- js/nav.js
+++ js/nav.js
@@ -35,3 +35,20 @@ let scrollh = () => {
 
 window.addEventListener("scroll", scrollh);
 
+const swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
