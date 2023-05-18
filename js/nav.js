1a90cf9162ba0bce66570b7e98293e4e2525fa9e
sonusingh <sonusinghcom926@gmail.com>
2023-05-18 20:18:47 +0530

    kk

--- js/nav.js
+++ js/nav.js
@@ -54,7 +54,7 @@ const swiper = new Swiper(".mySwiper", {
     },
 });
 
-//owl*js***************//
+//owl*js*****************************************************************************************//
 
 $(".carousel").owlCarousel({
     margin: 20,
@@ -78,31 +78,4 @@ $(".carousel").owlCarousel({
     }
 });
 
-/***/
-
-const accordionContent = document.querySelectorAll(".accordion-content");
-accordionContent.forEach((item, index) => {
-    let header = item.querySelector("header");
-    header.addEventListener("click", () => {
-        item.classList.toggle("open");
-        let description = item.querySelector(".description");
-        if (item.classList.contains("open")) {
-            description.style.height = `${description.scrollHeight}px`; //scrollHeight property returns the height of an element including padding , but excluding borders, scrollbar or margin
-            item.querySelector("i").classList.replace("fa-plus", "fa-minus");
-        } else {
-            description.style.height = "0px";
-            item.querySelector("i").classList.replace("fa-minus", "fa-plus");
-        }
-        removeOpen(index);
-    })
-})
-function removeOpen(index1) {
-    accordionContent.forEach((item2, index2) => {
-        if (index1 != index2) {
-            item2.classList.remove("open");
-            let des = item2.querySelector(".description");
-            des.style.height = "0px";
-            item2.querySelector("i").classList.replace("fa-minus", "fa-plus");
-        }
-    })
-}
+/******************************************************************************************/
