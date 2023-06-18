80e8210aa9a6697259e85b414b649cd30f5ccf94
sonu singh <sonusinghcom926@gmail.com>
2023-06-18 22:53:30 +0530

    kk

--- js/nav.js
+++ js/nav.js
@@ -58,7 +58,7 @@ const swipe = new Swiper(".mySwipe", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-        delay: 2500,
+        delay: 3500,
         disableOnInteraction: false,
     },
     pagination: {
